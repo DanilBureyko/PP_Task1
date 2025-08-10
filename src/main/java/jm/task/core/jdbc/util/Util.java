285c069103657e029e304951bf56b9e238b166b6
DanilBureyko <116087625+DanilBureyko@users.noreply.github.com>
2025-08-10 20:31:38 +0300

    END JDBC

---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,26 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Util {
+    private static String url = "jdbc:mysql://localhost:3306";
+    private static String user = "root";
+    private static String password = "root";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try{
+            if(connection == null || connection.isClosed()){
+                connection = DriverManager.getConnection(url, user, password);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+
+    }
+}
